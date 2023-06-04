e0404bf25046bd654ab83437e8b97d628cc7ab80
Bohdan9924 <bohdanminiailo22@gmail.com>
2023-06-04 19:19:07 +0200

    done 2 task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Player from "@vimeo/player";
+import throttle from "lodash.throttle";
 
+const videoEl = document.querySelector("#vimeo-player");
+
+const player = new Player(videoEl);
+
+player.on("timeupdate", throttle(data => {
+    localStorage.setItem("videoplayer-current-time", data.seconds);
+}, 1000));
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time')).then(function (seconds) {}).catch(function (error) {
+    switch (error.name) {
+        case 'RangeError':
+            break;
+            default:
+            break;
+    }
+});
