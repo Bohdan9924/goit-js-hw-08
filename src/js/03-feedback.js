6dc2af6991861c05dd1f3025e0f309a93986db98
Bohdan9924 <bohdanminiailo22@gmail.com>
2023-06-04 20:17:17 +0200

    done 3 task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,31 @@
+import throttle from 'lodash.throttle';
 
+const formEl = document.querySelector('.feedback-form');
+
+formEl.addEventListener('input', throttle(() => {
+  const formObj = {
+    email: formEl.email.value,
+    message: formEl.message.value,
+  };
+  localStorage.setItem('feedback-form-state', JSON.stringify(formObj));
+}, 500));
+
+const localForm = localStorage.getItem('feedback-form-state');
+
+if (localForm) {
+    const { email, message } = JSON.parse(localForm);
+    formEl.email.value = email,
+        formEl.message.value = email;
+}
+
+formEl.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    formEl.email.value = '';
+    formEl.message.value = '';
+    localStorage.setItem('feedback-form-state', JSON.stringify({
+        email: '',
+        message: '',
+    }));
+    console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
+});
